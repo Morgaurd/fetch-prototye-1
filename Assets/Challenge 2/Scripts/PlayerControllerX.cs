67f04a598361e38a12824e7205a988d0d7a45f39
Morgaurd <111660005+Morgaurd@users.noreply.github.com>
2022-09-01 08:51:01 -0700

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject[] dogPrefab;
+    private float count = 0.0f;
+    public float maxSpawn = 0.5f;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        count = maxSpawn;
+    }
+    void Update()
+    {
+        // On spacebar press, send dog
+        count += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && count >= maxSpawn )
+        {
+            Instantiate(dogPrefab[0], transform.position, dogPrefab[0].transform.rotation);
+            count = 0;
+        }
+
+
+    }
+}
